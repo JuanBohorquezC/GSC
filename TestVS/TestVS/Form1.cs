6a3883a0a29de6e95664bdda9aaff381321418c6
JuanBohorquezC <Juan.Bohorquez@ecopetrol.com.co>
2018-09-11 18:47:36 -0500

    Cambio de Mensaje

--- TestVS/TestVS/Form1.cs
+++ TestVS/TestVS/Form1.cs
@@ -19,7 +19,7 @@ namespace TestVS
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola");
+            MessageBox.Show("Hola Mundo");
         }
     }
 }
